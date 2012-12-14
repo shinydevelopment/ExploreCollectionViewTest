f58f5e549e12fb85180e96302edd7261de0818cf
Dave Verwer <dave.verwer@shinydevelopment.com>
2012-12-14 18:37:09 +0000

    Fixed up anchor point issue.

--- ExploreCollectionViewTest/PhotoGridCollectionViewCell.m
+++ ExploreCollectionViewTest/PhotoGridCollectionViewCell.m
@@ -8,7 +8,7 @@
   [super applyLayoutAttributes:attributes];
   self.layer.anchorPoint = attributes.anchorPoint;
   CGPoint position = self.layer.position;
-  position.x += (attributes.anchorPoint.x > 0.5) ? self.layer.bounds.size.width : -self.layer.bounds.size.width;
+  position.x += (attributes.anchorPoint.x > 0.5) ? self.layer.bounds.size.width / 2 : -self.layer.bounds.size.width / 2;
   self.layer.position = position;
 }
 
