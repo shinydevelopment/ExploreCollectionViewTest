1885f51487808ac72a75287e634414f5e07683f9
Dave Verwer <dave.verwer@shinydevelopment.com>
2012-12-14 18:28:13 +0000

    WIP on positioning the cell with an anchor point.

--- ExploreCollectionViewTest/PhotoGridCollectionViewCell.m
+++ ExploreCollectionViewTest/PhotoGridCollectionViewCell.m
@@ -3,4 +3,13 @@
 
 @implementation PhotoGridCollectionViewCell
 
+- (void)applyLayoutAttributes:(PhotoGridViewLayoutAttributes *)attributes
+{
+  [super applyLayoutAttributes:attributes];
+  self.layer.anchorPoint = attributes.anchorPoint;
+  CGPoint position = self.layer.position;
+  position.x += (attributes.anchorPoint.x > 0.5) ? self.layer.bounds.size.width : -self.layer.bounds.size.width;
+  self.layer.position = position;
+}
+
 @end
