16e5fd090f3f1f6eeed00287c11ac5de7244bf7b
Dave Verwer <dave.verwer@shinydevelopment.com>
2012-12-14 18:28:01 +0000

    Fixed bug where custom attributes would be lost during attribute copies.

--- ExploreCollectionViewTest/PhotoGridViewLayoutAttributes.m
+++ ExploreCollectionViewTest/PhotoGridViewLayoutAttributes.m
@@ -2,4 +2,12 @@
 
 @implementation PhotoGridViewLayoutAttributes
 
+- (id)copyWithZone:(NSZone *)zone
+{
+  PhotoGridViewLayoutAttributes *attributes = [super copyWithZone:zone];
+  attributes.anchorPoint = self.anchorPoint;
+  attributes.shadowAlpha = self.shadowAlpha;
+  return attributes;
+}
+
 @end
